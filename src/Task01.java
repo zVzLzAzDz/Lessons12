28b374609b13d023c6956ce0bde4f6818976a624
Viktor Ivanchenko <ivanvikvik@mail.ru>
2022-09-08 09:31:47 +0300

    Updated Task01

--- src/Task01.java
+++ src/Task01.java
@@ -1,5 +1,9 @@
 public class Task01 {
     public static void main(String[] args) {
         System.out.println("Testing GitHub!!!");
+        System.out.println("Testing GitHub!!!");
+        System.out.println("Testing GitHub!!!");
+        System.out.println("Testing GitHub!!!");
+        System.out.println("Testing GitHub!!!");
     }
 }
