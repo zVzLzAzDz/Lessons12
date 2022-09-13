197bc540fa756b0953538ea19793f14e82c050e8
Viktor Ivanchenko <ivanvikvik@mail.ru>
2022-09-13 11:40:17 +0300

    Static method

---
+++ src/Task04.java
@@ -0,0 +1,15 @@
+public class Task04 {
+
+    static void sum(){
+        // logic
+        int result = 5 + 6;
+        System.out.println(result);
+    }
+
+
+
+    public static void main(String[] args) {
+        // output
+        sum();
+    }
+}
---
+++ src/Task05.java
@@ -0,0 +1,5 @@
+public class Task05 {
+    public static void main(String[] args) {
+        Task04.sum();
+    }
+}
