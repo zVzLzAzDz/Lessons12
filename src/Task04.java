33fb74dc41619fdb0efaa070e154046fe08a7f14
Viktor Ivanchenko <ivanvikvik@mail.ru>
2022-09-13 11:57:02 +0300

    prob

--- src/Task04.java
+++ src/Task04.java
@@ -1,15 +1,21 @@
 public class Task04 {
 
-    static void sum(){
+    static void sum() {
         // logic
         int result = 5 + 6;
+    }
+
+    static void print(int result) {
         System.out.println(result);
     }
 
 
-
     public static void main(String[] args) {
         // output
-        sum();
+        print(2);
+        print(5);
+        print(88);
+        print(888);
+
     }
 }
---
+++ src/Task06.java
@@ -0,0 +1,19 @@
+public class Task06 {
+
+    static int sum(int a, int b) {
+        // logic
+        int result = a + b;
+        return result;
+    }
+
+    static void print(int result) {
+        // output
+        System.out.println(result);
+    }
+
+
+    public static void main(String[] args) {
+        int result = sum(56, 7);
+        print(result);
+    }
+}
